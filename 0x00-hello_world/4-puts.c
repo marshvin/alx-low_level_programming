a4487c1794109ec70e7e4e8e4d077751c32cb47d
marshvin <vincentmarsh01@gmail.com>
2022-06-02 12:19:12 -0700

    C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+
+ *main - puts the string into the output
+ *put - displays the output
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
