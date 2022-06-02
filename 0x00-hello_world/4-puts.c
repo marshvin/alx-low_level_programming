d895c57b9ab52ce7233590de338b689e6afb217d
marshvin <vincentmarsh01@gmail.com>
2022-06-02 13:18:22 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
 *main - puts the string into the output
 *put - displays the output
-*Return (0) - shows function does not return a value
+*Return (0) : shows function does not return a value
 */
 int main(void)
 {
