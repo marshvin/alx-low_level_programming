0a66a093af46c72726839c755c09d3d5d22ce892
marshvin <vincentmarsh01@gmail.com>
2022-06-02 13:51:17 -0700

    C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - puts the string into the output
+*put - displays the output
+*Return: 0 shows function does not return a value
+*/
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,")
+return (0);
+}
