5bc3fc4008059a1d77a591cd8c57432055e70449
marshvin <vincentmarsh01@gmail.com>
2022-06-02 12:22:34 -0700

    firstc program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-
- *main - puts the string into the output
- *put - displays the output
- */
+*main - puts the string into the output
+*put - displays the output
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
