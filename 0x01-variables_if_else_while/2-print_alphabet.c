a3559e2f11e3dfd61f6691bba06333fd9e139c07
marshvin <vincentmarsh01@gmail.com>
2022-06-05 10:32:47 -0700

    C prog

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints letters of the alphabet
+* Return: 0 for successful exit status
+*/
+int main(void)
+{
+char c = 'a';
+do {
+putchar(c);
+c++;
+}
+while (c <= 'z');
+putchar('\n');
+return (0);
+}
