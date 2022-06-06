602e735fc038cb479ee193db45ed954f17f59828
marshvin <vincentmarsh01@gmail.com>
2022-06-06 10:11:18 -0700

    c prog

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - prints letters of the alphabet in lowercase & uppercase
+* Return: 0 for success exit status
+*/
+int main(void)
+{
+char cl = 'a';
+char cu = 'A';
+while (cl <= 'z')
+{
+putchar(cl);
+cl++;
+}
+while (cu <= 'Z'
+{
+putchar(cu);
+cu++;
+}
+putchar('\n');
+return (0);
+}
