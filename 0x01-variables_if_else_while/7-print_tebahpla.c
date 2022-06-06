9e8fe573c8e924648c9c8ed0c860ebbc60efa738
marshvin <vincentmarsh01@gmail.com>
2022-06-06 13:37:55 -0700

    c prog

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#tdio.h>
+/**
+* main - prints lowercase letters of the alphabet in reverse
+* Return: 0 for successful exit status
+*/
+int main(void)
+{
+char c = 'z';
+do {
+putchar(c);
+c--;
+} while (c >= 'a');
+putchar('\n');
+return (0); 
+}
