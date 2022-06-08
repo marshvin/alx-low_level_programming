d3a978856aed50adc1cf64d38c055f73c5f2e2a5
marshvin <vincentmarsh01@gmail.com>
2022-06-08 03:01:16 -0700

    c prog

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabetic character
+* @c: letter to be checked
+*
+* Return: 1 if letter is lowercase. 0 otherwise
+*/
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+} 
+}
