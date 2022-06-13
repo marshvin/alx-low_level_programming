de3f28bca6af1da7982a4c996ac4470da9183a3c
marshvin <vincentmarsh01@gmail.com>
2022-06-13 12:21:51 -0700

    c prog

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * rev_string - reverse a string
+ * print_rev - reverse a string
  *@s: string to reverse
  *Return: Nothing
 */
-void print_rev(char *s)  
+void print_rev(char *s)
 {
 int l;
 l = 0;
