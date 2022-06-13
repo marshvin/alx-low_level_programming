4b6f1ecb9d4dfb394123f05e9e6ab274ca18f566
marshvin <vincentmarsh01@gmail.com>
2022-06-13 09:44:29 -0700

    c prog

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,11 +5,11 @@
  * @b: second parameter
  *
  * Description: swap_int swaps the value of *a and *b
- */
+*/
 void swap_int(int *a, int *b) 
 {
-  int c;
-  c = *a;
-  *a = *b;
-  *b = c;
+int c;
+c = *a;
+*a = *b;
+*b = c;
 }
