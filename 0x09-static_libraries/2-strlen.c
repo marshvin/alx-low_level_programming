87c022642b15eca42f72d84cf5f1164ec935a7d4
marshvin <vincentmarsh01@gmail.com>
2022-06-27 12:02:11 -0700

    commit

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+
+ * _strlen - returns the length of a string
+
+ * @s: first parameter
+
+ *
+
+ * Description: returns the length of the string
+
+ * Return: the length of the string
+
+ */
+
+int _strlen(char *s)
+  
+{
+  
+  int l = 0;
+  
+
+  
+  while (*s++)
+    
+    l++;
+  
+  return (l);
+  
+}
