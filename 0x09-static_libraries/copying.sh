3109485177fb667bab0167c9f2a7e44995a22f2e
marshvin <vincentmarsh01@gmail.com>
2022-06-29 10:18:52 -0700

    yeah

---
+++ 0x09-static_libraries/copying.sh
@@ -0,0 +1,7 @@
+cp ./alxschool-low_level_programming/0x04-more_functions_nested_loops/0-isupper.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+
+cp ./alxschool-low_level_programming/0x07-pointers_arrays_strings/0-memset.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+
+cp ./alxschool-low_level_programming/0x06-pointers_arrays_strings/0-strcat.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
+
+cp ./alxschool-low_level_programming/0x04-more_functions_nested_loops/1-isdigit.c /home/developer/alxschool-low_level_programming/0x09-static_libraries
