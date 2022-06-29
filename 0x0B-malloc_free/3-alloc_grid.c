c9e6735cd1aaa8085557aa3c5e4d32e5ca37247e
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2022-06-29 19:04:14 +0300

    new task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,5 @@
-#include "holberton.h"
-
+#include "main.h"
 #include <stdlib.h>
-
 #include <stdio.h>
 
 /**
@@ -15,87 +13,39 @@
  *Return: 0 (success)
 
  */
-
 int **alloc_grid(int width, int height)
-  
 {
-  
-  int **grid;
-  
-  int i, j, k, l;
-  
-
-  
-  /**verify if with or height is 0*/
-  
-  if (width <= 0 || height <= 0)
-    
-    return (NULL);
-  
-
-  
-  /**asign size*/
-  
-  grid = malloc(height * sizeof(int *));
-  
-
-  
-  /**verify if my pointer is null*/
-  
-  if (grid == NULL)
-    
-    return (NULL);
-  
-
-  
-  /**Modify the size*/
-  
-  for (i = 0; i < height; i++)
-    
-    {
-      
-      grid[i] = malloc(width * sizeof(int));
-      
-      if (grid[i] == NULL)
-	
-	{
-	  
-	  for (j = 0; j < i; j++)
-	    
-	    {
-	      
-	      free(grid[j]);
-	      
-	    }
-	  
-	  free(grid);
-	  
-	  return (NULL);
-	  
-	}
-      
-    }
-  
-
-  
-  /**fill the grid*/
-  
-  for (k = 0; k < height; k++)
-    
-    {
-      
-      for (l = 0; l < width; l++)
-	
-	{
-	  
-	  grid[k][l] = 0;
-	  
-	}
-      
-    }
-  
-
-  
-  return (grid);
-  
+int **grid;
+int i, j, k, l;
+/**verify if with or height is 0*/
+if (width <= 0 || height <= 0)
+return (NULL);
+/**asign size*/
+grid = malloc(height * sizeof(int *));
+/**verify if my pointer is null*/
+if (grid == NULL)
+return (NULL);
+/**Modify the size*/
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+{
+free(grid[j]);
+}
+free(grid);
+return (NULL);
+}
+}
+/**fill the grid*/
+for (k = 0; k < height; k++)
+{
+for (l = 0; l < width; l++)
+{
+grid[k][l] = 0;
+}
+}
+return (grid);
 }
