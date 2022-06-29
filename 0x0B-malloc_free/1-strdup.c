9388a72484ce43566c256ceb753b49d2c1eb5e17
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2022-06-29 18:52:07 +0300

    new task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,67 +1,29 @@
-#include "holberton.h"
-
+#include "main.h"
 #include <stdlib.h>
-
 #include <stdio.h>
-
 /**
-
  * *_strdup - duplicaate a strning with new memory
-
  *@str: string received
-
  *Return: Null otherwise the duplicated string
-
- */
-
+*/
 char *_strdup(char *str)
-  
 {
-  
-  char *cp;
-  
-  unsigned int i, j;
-  
-
-  
-  if (str == NULL)
-    
-    return (NULL);
-  
-  for (i = 0; str[i] != '\0'; i++)
-    
-    {
-      
-    }
-  
-
-  
-  i++;
-  
-  cp = malloc(i * sizeof(char));
-  
-
-  
-  if (cp == NULL)
-    
-    {
-      
-      return (NULL);
-      
-    }
-  
-
-  
-  for (j = 0; j < i; j++)
-    
-    {
-      
-      cp[j] = str[j];
-      
-    }
-  
-
-  
-  return (cp);
-  
+char *cp;
+unsigned int i, j;
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+i++;
+cp = malloc(i * sizeof(char));
+}
+if (cp == NULL)
+{
+return (NULL);
+}
+for (j = 0; j < i; j++)
+{
+cp[j] = str[j];
+}
+return (cp);
 }
