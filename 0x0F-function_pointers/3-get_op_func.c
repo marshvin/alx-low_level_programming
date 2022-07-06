41a0085946a4379ec4e0d32be46c32c271dcce0d
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2022-07-06 23:53:57 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,27 @@
 #include "3-calc.h"
-
 /**
  * get_op_func - returns a pointer to the function that corresponds to
  * given operator
  * @s: operator
  *
  * Return: corresponding function. NULL otherwise
- */
-
+*/
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while (ops[i].op)
-	{
-		if (strcmp(ops[i].op, s)  == 0)
-			return (ops[i].f);
-		i++;
-	}
-
-	return (NULL);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op)
+{
+if (strcmp(ops[i].op, s)  == 0)
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
